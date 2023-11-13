18ca14c46924cec2ac09daad4adf077f85300803
wasim Haider55 <wasimhaider14379@gmail.com>
2023-11-13 11:24:03 +0500

    some changing

--- countDownTime/app.js
+++ countDownTime/app.js
@@ -1,4 +1,4 @@
-const endDate = "30 November 2023 12:10 PM";
+const endDate = "14 November 2023 04:00 PM";
 document.getElementById("end-date").innerText = endDate;
 const input = document.querySelectorAll("input");
 
@@ -6,24 +6,24 @@ const clock = () => {
     const end = new Date(endDate);
     const now = new Date();
     const different = (end - now) / 1000;
-   if(different < 0 ) return;
-   
+    if (different < 0) return;
+
     input[0].value = (Math.floor(different / 3600 / 24));
-    input[1].value = (Math.floor((different/ 3600) % 24 / 12));
-    input[2].value = (Math.floor((different/ 60) % 60));
+    input[1].value = (Math.floor((different / 3600) % 24));
+    input[2].value = (Math.floor((different / 60) % 60));
     input[3].value = (Math.floor((different % 60)));
 }
 
 clock();
 
-setInterval( 
+setInterval(
     () => {
-        clock() 
-},100
+        clock()
+    }, 100
 
 )
 
 
-    
 
-    
+
+
